dfbe4e7761717216a022a3fe70ab686ee1d7272f
Igor Usenko <usenkoigor01@gmail.com>
2024-06-11 18:42:43 +0300

    Разработка завершена

--- Client.py
+++ Client.py
@@ -1,7 +1,11 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.0.11', 2021))
+client.connect(('192.168.1.3', 2021))
+
+# Получение ответа от сервера
+response = client.recv(1024)
+print(f"Сервер ответил: {response.decode()}")
 
 while True:
     client.send((input().encode("utf-8")))
--- Server.py
+++ Server.py
@@ -1,16 +1,22 @@
 import socket
-import webbrowser
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((socket.gethostbyname_ex(socket.gethostname())[-1][-1], 2021))
 server.listen()
 
+# Получение локального IP-адреса сервера
+local_ip = socket.gethostbyname_ex(socket.gethostname())[-1][-1]
+print(f"Сервер запущен на локальном IP-адресе: {local_ip}")
+
 while True:
-    user, adres = server.accept()
+    client, address = server.accept()
+    print(f"Новое подключение от {address}")
+
+    # Отправка сообщения "готов к работе" клиенту
+    client.send("Готов к работе".encode("utf-8"))
 
     while True:
-        data = user.recv(1024).decode("utf-8").lower()
-        print(data)
-
-        # Команды
-        if data =="сайт":
-            webbrowser.open("https://it-mir.info/")
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Получено сообщение: {data.decode()}")
